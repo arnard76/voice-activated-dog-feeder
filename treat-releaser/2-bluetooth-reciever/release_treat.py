ac0423f03a573017e0057351b6a59e79f965b2bc
Arnav <arnard76@gmail.com>
2024-08-01 21:49:32 +1200

    reverse motor direction for correct piston movement

--- treat-releaser/2-bluetooth-reciever/release_treat.py
+++ treat-releaser/2-bluetooth-reciever/release_treat.py
@@ -9,11 +9,11 @@ def release_treat():
     led_pin.on()
     time.sleep(2)
     
-    for i in range(180,0,-10):
+    for i in range(0,180,30):
         servo_Angle(s1, i)
         time.sleep(0.05)
         
-    for i in range(0,180,10):
+    for i in range(180,0,-10):
         servo_Angle(s1, i)
         time.sleep(0.05)
         
